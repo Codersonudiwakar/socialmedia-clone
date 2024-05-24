10c6204dee75319c5253ba9f65f5b40165e51eba
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-05-24 21:31:07 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+
+
+const Home=()=>{
+
+    return (
+<div className="body">
+        <div className='head'>
+<Logo/>
+        </div>
+    
+        <div className="split-container">
+          <div className="left">
+            
+          </div>
+          <div className="middle">
+           <ChatInput/>
+          </div>
+          <div class="right">
+          
+          </div>
+          </div>
+
+    </div>
+    );
+}
