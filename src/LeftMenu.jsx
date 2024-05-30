fadb861a6a20ee97dc252180b720e84a818bb4c4
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-05-30 23:55:36 +0530

    new

--- src/LeftMenu.jsx
+++ src/LeftMenu.jsx
@@ -6,23 +6,14 @@ import { CiCirclePlus } from "react-icons/ci";
 import { IoSettingsOutline } from "react-icons/io5";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { GrGroup } from "react-icons/gr";
-
-             
-
-
-
-
-
-
 const Leftmenu=()=>{
-
     return (
 <div className="Leftmenu">
         
         <div className="sidebar">
       <ul>
         <li><a href="#home"><FaHome />   Home</a></li>
-        <li><a href="#services"><CiCirclePlus />   Services</a></li>
+        <li><a href="#services"><CiCirclePlus />   Post</a></li>
         <li><a href="#clients"><IoMdNotificationsOutline />   Notification</a></li>
         <li><a href="#contact"><IoSettingsOutline /> Setting</a></li>
         <li><a href="#contact"><GrGroup />  Your Network</a></li>
